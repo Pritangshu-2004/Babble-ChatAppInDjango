0f816f902758d9fb2e0a7d33a962cc41fd802166
Pritangshu <2004.pritangshu@gmail.com>
2025-04-13 12:20:17 +0530

    changed

--- WebChat/settings.py
+++ WebChat/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sd^^-r5edl)&01&9r0d)#&m)()+41k@npr^kx-6-lk9h68b(r@'
+SECRET_KEY = 'django-insecure-sd^^-r5edl)&01&9r0d)#&m)()+41k@npr^kx-6-lk9h68b(r@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
